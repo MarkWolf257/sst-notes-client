7e48b6885170a88f2bb7d87377e487730ce890f8
MarkWolf257 <sambhunathmeher@gmail.com>
2023-07-04 21:51:16 +0530

    Added a feature to create multiple notes in same page

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
 import { onError } from "../libs/errorLib";
@@ -9,29 +10,49 @@ import { API } from "aws-amplify";
 import { s3Upload } from "../libs/awsLib";
 
 export default function NewNote() {
-    const file = useRef(null);
     const navigate = useNavigate();
-    const [content, setContent] = useState("");
+    const [content, setContent] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [path, setPath] = useState("");
-
-    function validateForm() {
-        return content.length > 0;
-    }
+    const [path, setPath] = useState([]);
+    const [notes, setNotes] = useState([0]);
+    const [files, setFiles] = useState([]);
 
     function handleFileChange(event) {
-        file.current = event.target.files[0];
-        if (event.target.files.length !== 0 && file.current.type.includes('image')) {
-            setPath(URL.createObjectURL(event.target.files[0]));
-        } else {
-            setPath("");
+        const index = event.target.index;
+        files[index] = event.target.files[0];
+        setFiles((oldArray) => [...oldArray.slice(0, index), files[index], ...oldArray.slice(index+1)]);
+        if (event.target.files.length !== 0 && files[index].type.includes('image')) {
+            setPath((oldArray) => [...oldArray.slice(0, index), URL.createObjectURL(files[index]), ...oldArray.slice(index+1)]);
         }
     }
 
     async function handleSubmit(event) {
         event.preventDefault();
 
-        if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+        for (let index of notes) {
+            if (files[index] && files[index].size > config.MAX_ATTACHMENT_SIZE) {
+                alert(
+                    `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE / 1000000} MB.`
+                );
+                return;
+            }
+        }
+
+        setIsLoading(true);
+
+        try {
+            for (let index of notes) {
+                const attachment = files[index] ? await s3Upload(files[index]) : null;
+                await createNote({ content: content[index], attachment });
+            }
+        } catch (e) {
+            onError(e);
+            setIsLoading(false);
+        }
+
+        navigate("/");
+
+        /*if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
             alert(
                 `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE / 1000000} MB.`
             );
@@ -48,7 +69,7 @@ export default function NewNote() {
         } catch (e) {
             onError(e);
             setIsLoading(false);
-        }
+        }*/
     }
 
     function createNote(note) {
@@ -57,28 +78,49 @@ export default function NewNote() {
         });
     }
 
-    return (
-        <div className="NewNote">
-            <Form onSubmit={handleSubmit}>
+    function renderInput(index) {
+        return (
+            <div>
                 <Form.Group controlId="content">
                     <Form.Control
-                        value={content}
+                        value={content[index]}
                         as="textarea"
-                        onChange={(e) => setContent(e.target.value)}
+                        onChange={(e) => setContent((oldArray) => [...oldArray.slice(0, index), e.target.value, ...oldArray.slice(index+1)])}
                     />
                 </Form.Group>
                 <Form.Group controlId="file">
                     <Form.Label>Attachment</Form.Label>
-                    <Form.Control onChange={handleFileChange} type="file" />
+                    <Form.Control index={0} onChange={handleFileChange} type="file" />
                 </Form.Group>
-                {path && <img src={path} alt="" />}
+                {path[index] && <img src={path[index]} alt="" />}
+                
+            </div>
+        );
+    }
+
+    return (
+        <div className="NewNote">
+            <Form onSubmit={handleSubmit}>
+                {notes.map((index) => renderInput(index))}
+                <Button className="mb-4" onClick={() => setNotes((oldArray) => [...oldArray, oldArray.length])}>Add Note</Button>
+                <Button variant="danger" disabled={notes.length < 2} onClick={async function() {
+                    await setNotes((oldArray) => oldArray.slice(0, -1));
+                    setContent((oldArray) => oldArray.slice(0, notes.length-1));
+                    setFiles((oldArray) => oldArray.slice(0, notes.length-1));
+                    setPath((oldArray) => oldArray.slice(0, notes.length-1))
+                }}>Remove Note</Button>
                 <LoaderButton
                     block
                     type="submit"
                     size="lg"
                     variant="primary"
                     isLoading={isLoading}
-                    disabled={!validateForm()}
+                    disabled={function() {
+                        for (let p of content) {
+                            if (!p) return true;
+                        }
+                        return false;
+                    }}
                 >
                     Create
                 </LoaderButton>
